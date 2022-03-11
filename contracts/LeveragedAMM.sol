3e75be2c412de3bdc6b804a727df88eda24423ec
Jason Yu <onionf91@gmail.com>
2022-03-11 09:32:56 +0800

    Implement position estimation functions

--- contracts/LeveragedAMM.sol
+++ contracts/LeveragedAMM.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LeveragedAMM is Ownable {
     IERC20 public immutable tokenUsdc;
+    uint256 public immutable maxLeverage;
 
     uint256 public reserveUsdc;
     uint256 public reserveEth;
@@ -28,6 +29,7 @@ contract LeveragedAMM is Ownable {
 
     constructor(address tokenUsdc_) Ownable() {
         tokenUsdc = IERC20(tokenUsdc_);
+        maxLeverage = 10;
     }
 
     function usdcCashOf(address account) public view returns(uint256) {
@@ -38,12 +40,32 @@ contract LeveragedAMM is Ownable {
         return _ethCash[account];
     }
 
-    function depositUSDC(uint256 amount) public {
+    function depositUsdc(uint256 amount) public {
         tokenUsdc.transferFrom(_msgSender(), address(this), amount);
         if (owner() == _msgSender()) {
             reserveUsdc += amount;
         } else {
             _usdcCash[_msgSender()] += amount;
         }
+    }
+
+    function _calculateSwappedEth(uint256 usdcIn) internal view returns(uint256) {
+        require(usdcIn < reserveUsdc, "LeveragedAMM: out of reserve usdc.");
+        return reserveEth - ((reserveUsdc * reserveEth) / (reserveUsdc + usdcIn));
+    }
+
+    function _calculateSwappedUsdc(uint256 ethIn) internal view returns(uint256) {
+        require(ethIn < reserveEth, "LeveragedAMM: out of reserve eth.");
+        return reserveUsdc - ((reserveUsdc * reserveEth) / (reserveEth + ethIn));
+    }
+
+    function estimateEthPosition(uint256 usdc, uint256 leverage) public view returns(uint256) {
+        require(leverage <= maxLeverage, "LeveragedAMM: max leverage is 10.");
+        return _calculateSwappedEth(usdc * leverage);
+    }
+
+    function estimateUsdcPosition(uint256 eth, uint256 leverage) public view returns(uint256) {
+        require(leverage <= maxLeverage, "LeveragedAMM: max leverage is 10.");
+        return _calculateSwappedUsdc(eth * leverage);
     }
 }
--- test/LeveragedAMM.test.js
+++ test/LeveragedAMM.test.js
@@ -1,5 +1,6 @@
 const LAMM = artifacts.require('LeveragedAMM')
 const MockUSDC = artifacts.require('MockUSDC')
+const catchRevert = require("./exceptions.js").catchRevert;
 
 contract('LeveragedAMM', (accounts) => {
     let lAmm, usdc, owner, alice
@@ -12,32 +13,32 @@ contract('LeveragedAMM', (accounts) => {
     it('Should add eth liquidity when owner deposit eth to LAMM.', async () => {
         lAmm = await LAMM.deployed()
 
-        await web3.eth.sendTransaction({to: lAmm.address, from: owner, value: web3.utils.toWei("2", "ether")})
+        await web3.eth.sendTransaction({to: lAmm.address, from: owner, value: web3.utils.toWei('2', 'ether')})
 
         let balance = await web3.eth.getBalance(lAmm.address);
-        assert.equal(web3.utils.fromWei(balance.valueOf(), "ether"), 2)
+        assert.equal(web3.utils.fromWei(balance.valueOf(), 'ether'), 2)
 
         let reserveEth = await lAmm.reserveEth.call()
-        assert.equal(reserveEth.valueOf(), web3.utils.toWei("2", "ether"))
+        assert.equal(reserveEth.valueOf(), web3.utils.toWei('2', 'ether'))
     })
 
     it('Should add eth cash to user when deposit eth to LAMM.', async () => {
         lAmm = await LAMM.deployed()
 
-        await web3.eth.sendTransaction({to: lAmm.address, from: alice, value: web3.utils.toWei("0.1", "ether")})
+        await web3.eth.sendTransaction({to: lAmm.address, from: alice, value: web3.utils.toWei('0.1', 'ether')})
 
         let balance = await web3.eth.getBalance(lAmm.address);
-        assert.equal(web3.utils.fromWei(balance.valueOf(), "ether"), 2.1)
+        assert.equal(web3.utils.fromWei(balance.valueOf(), 'ether'), 2.1)
 
         let ethCash = await lAmm.ethCashOf.call(alice)
-        assert.equal(ethCash.valueOf(), web3.utils.toWei("0.1", "ether"))
+        assert.equal(ethCash.valueOf(), web3.utils.toWei('0.1', 'ether'))
     })
 
     it('Should add usdc liquidity when owner deposit usdc to LAMM.', async () => {
         usdc = await MockUSDC.deployed()
 
         await usdc.approve(lAmm.address, 5000, {from: owner})
-        await lAmm.depositUSDC(5000, {from: owner})
+        await lAmm.depositUsdc(5000, {from: owner})
 
         let balance = await usdc.balanceOf.call(lAmm.address)
         assert.equal(balance.valueOf(), 5000)
@@ -51,12 +52,38 @@ contract('LeveragedAMM', (accounts) => {
 
         await usdc.transfer(alice, 100, {from: owner})
         await usdc.approve(lAmm.address, 50, {from: alice})
-        await lAmm.depositUSDC(50, {from: alice})
+        await lAmm.depositUsdc(50, {from: alice})
 
         let balance = await usdc.balanceOf.call(lAmm.address)
         assert.equal(balance.valueOf(), 5050)
 
         let usdcCash = await lAmm.usdcCashOf.call(alice)
         assert.equal(usdcCash.valueOf(), 50)
+    })
+
+    it('Estimate eth position should calculate position by AMM with leverage.', async () => {
+        let ethPosition = await lAmm.estimateEthPosition.call(200, 5)
+        assert.equal(web3.utils.fromWei(ethPosition.valueOf(), 'ether'), 0.333333333333333334)
+    })
+
+    it('Estimate eth position should revert while leverage exceed max value.', async () => {
+        await catchRevert(lAmm.estimateEthPosition.call(50, 11))
+    })
+
+    it('Estimate eth position should revert while leveraged usdc exceed reserve usdc.', async () => {
+        await catchRevert(lAmm.estimateEthPosition.call(4000, 2))
+    })
+
+    it('Estimate usdc position should calculate position by AMM with leverage.', async () => {
+        let usdcPosition = await lAmm.estimateUsdcPosition.call(web3.utils.toWei('0.01', 'ether'), 8)
+        assert.equal(usdcPosition.valueOf(), 193)
+    })
+
+    it('Estimate usdc position should revert while leverage exceed max value.', async () => {
+        await catchRevert(lAmm.estimateUsdcPosition.call(web3.utils.toWei('0.01', 'ether'), 12))
+    })
+
+    it('Estimate usdc position should revert while leveraged eth exceed reserve eth.', async () => {
+        await catchRevert(lAmm.estimateUsdcPosition.call(web3.utils.toWei('1.0', 'ether'), 3))
     })
 })
